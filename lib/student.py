e5c8e384e0017e4f1eb5a7ecb6b353fd420c46a2
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-17 14:01:06 +0300

    pt 2 installed

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,39 @@
+# class Student:
+#     pass
+
+# class ChattyStudent(Student):
+#     pass
+
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for x in range(0,10):
+            super().raise_hand()
+
+
+# Create instances of Student and ChattyStudent
+student = Student()
+chatty_student = ChattyStudent()
+
+
+# Call methods on Student instance
+print("Student says hello:")
+student.hello()
+print("\nStudent raises hand:")
+student.raise_hand()
+
+# Call methods on ChattyStudent instance
+print("\nChattyStudent says hello:")
+chatty_student.hello()
+print("\nChattyStudent raises hand:")
+chatty_student.raise_hand()
